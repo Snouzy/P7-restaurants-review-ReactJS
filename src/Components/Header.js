0ae8f96f5731c6ca05f1a85678d2a3fb6219fbaa
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2019-12-09 23:03:10 +0100

    finish the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
-import {HeaderStyle, ImgHeader} from "./Header.style";
-import logo from '../imgs/snouzEat128x128_blanc.png'
+import { NavWrapper, ImgWrapper } from "./Header.style";
+import logo from "../imgs/snouzEat128x128_blanc.png";
 // import PStyle from "./Header.style";
 
 class Header extends Component {
-  render() {
-    return (
-      <HeaderStyle>
-        <ImgHeader src={logo} alt="Snouz'Eat logo's"/>
-        Mangez-mieux avec Snouz'Eat !
-      </HeaderStyle>
-    );
-  }
+   render() {
+      return (
+         <NavWrapper className="navbar navbar-dark bg-dark mb-2">
+            <ImgWrapper>
+               <img src={logo} alt="brand logo"/>
+            </ImgWrapper>
+            <h3 className="navbar-text text-center">Mangez mieux avec SNOUZ'EAT !</h3>
+         </NavWrapper>
+      );
+   }
 }
 
 export default Header;
--- src/Components/Map.style.js
+++ src/Components/Map.style.js
@@ -1,1 +1,13 @@
-import styled from styled-components;
+import styled from "styled-components";
+
+const DivMap = styled.div`
+   display: flex;
+   width: 80%;
+   background-color: #343a40;
+   text-align: center;
+   color: white;
+   height: 20%;
+   font-size: 5vw;
+`;
+
+export { DivMap };
