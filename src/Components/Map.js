23e3c3617762235efadc1c1924508b1e7ea81ab7
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2019-12-12 11:58:26 +0100

    adding the markers

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,22 +1,44 @@
 import React, { Component } from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
-
+import {
+   GoogleMap,
+   withScriptjs,
+   withGoogleMap,
+   Marker
+} from "react-google-maps";
+import data from "../data.json";
 class Map extends Component {
-   Map() {
+   state = {
+      selectedPark: null
+   }
+
+   handleClick(restaurant) {
+      
+   }
+
+   Map = () => {
       return (
          <GoogleMap
             defaultZoom={8}
+            //Strasbourg
             defaultCenter={{
                lat: 48.58392,
                lng: 7.74553
             }}
-         />
+         >
+            {data.map(resto => (
+               <Marker
+                  key={resto.restaurantName}
+                  position={{ lat: resto.lat, lng: resto.long }}
+                  onClick={() => this.handleClick()}
+               />
+            ))}
+         </GoogleMap>
       );
    }
    render() {
       const WrappedMap = withScriptjs(withGoogleMap(this.Map));
       return (
-         <div className="col-sm-12 col-lg-9" style={{ height: "100vh" }}>
+         <div className="col-sm-12 col-lg-9" style={{ height: "50%" }}>
             <WrappedMap
                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
                loadingElement={<div style={{ height: `100%` }} />}
