6497e419ebb87b82ed475fa9893d3c52520ed9be
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2019-12-16 14:48:00 +0100

    adding the center position of the user onload

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Stars from "./Stars";
+import UserIcon from "../imgs/MapMarker_PushPin_Left_Green.svg";
 
 import {
    GoogleMap,
@@ -13,8 +14,32 @@ import data from "../data.json";
 import { averageStars } from "../services/libs";
 
 class Map extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         coords: {}
+      };
+   }
+
+   componentDidMount() {
+      this.getCurrentLocation();
+   }
+
+   getCurrentLocation() {
+      if (window.navigator && window.navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(pos => {
+            const { latitude, longitude } = pos.coords;
+            const coords = { lat: latitude, lng: longitude };
+            this.setState({ coords });
+         });
+      }
+   }
+
    render() {
-      const WrappedMap = withScriptjs(withGoogleMap(() => <RenderGoogleMap />));
+      const WrappedMap = withScriptjs(
+         withGoogleMap(() => <RenderGoogleMap coords={this.state.coords} />)
+      );
+
       return (
          <div className="col-sm-12 col-lg-9" style={{ height: "50%" }}>
             <WrappedMap
@@ -28,8 +53,11 @@ class Map extends Component {
    }
 }
 
-const RenderGoogleMap = () => {
-   const [selectedRestaurant, setSelectedRestaurant] = React.useState(null);
+//Child component
+const RenderGoogleMap = props => {
+   //on recoit les props de la classe Map
+   const { coords } = props;
+   const [selectedRestaurant, setSelectedRestaurant] = useState(null);
 
    const handleClick = index => {
       //put the selectedRestaurant into the state
@@ -40,11 +68,9 @@ const RenderGoogleMap = () => {
       <GoogleMap
          defaultZoom={8}
          //Sensushi for the moment (need to replace by the center of the user)
-         defaultCenter={{
-            lat: 47.58612,
-            lng: 7.5832
-         }}
+         defaultCenter={coords}
       >
+         {/* display the markers */}
          {data.map((resto, index) => {
             return (
                <Marker
@@ -57,7 +83,14 @@ const RenderGoogleMap = () => {
                />
             );
          })}
-
+         <Marker
+            key="positionUser"
+            position={coords}
+            icon={{
+               url: UserIcon,
+               scaledSize: new window.google.maps.Size(40, 40)
+            }}
+         />
          {selectedRestaurant && (
             <InfoWindow
                position={{
