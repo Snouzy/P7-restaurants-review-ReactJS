ea6735ae42ede54cef43abe93b1ffbcca1cb2917
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2019-12-21 00:33:49 +0100

    implementing google street view

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,6 +1,8 @@
 import React, { Component, useState, Fragment } from "react";
 import Stars from "./Stars";
 import UserIcon from "../imgs/MapMarker_PushPin_Left_Green.svg";
+import { API_KEY } from "../api_key";
+import ReactStreetview from "react-streetview";
 
 import {
    GoogleMap,
@@ -48,7 +50,7 @@ class Map extends Component {
       return (
          <div className="col-sm-12 col-lg-9" style={{ height: "50%" }}>
             <WrappedMap
-               googleMapURL={`https://maps.googleapis.com/maps/api/js?3.40.explibraries=geometry,drawing,places&key=`}
+               googleMapURL={`https://maps.googleapis.com/maps/api/js?3.40.explibraries=geometry,drawing,places&key=${API_KEY}`}
                loadingElement={<div style={{ height: `100%` }} />}
                containerElement={<div style={{ height: `700px` }} />}
                mapElement={<div style={{ height: `100%` }} />}
@@ -70,84 +72,110 @@ const RenderGoogleMap = props => {
    };
 
    return (
-      <GoogleMap defaultZoom={8} defaultCenter={coords}>
-         {/* display the markers */}
-         {data.map((resto, index) => {
-            return (
-               <Marker
-                  key={resto.restaurantName}
-                  position={{
-                     lat: resto.lat,
-                     lng: resto.long
-                  }}
-                  onClick={() => handleClick(index)}
-               />
-            );
-         })}
-         <Marker
-            key="positionUser"
-            position={coords}
-            icon={{
-               url: UserIcon,
-               scaledSize: new window.google.maps.Size(40, 40)
-            }}
-         />
-         {/* If the user selected a restaurant, we display the google modal : */}
-         {selectedRestaurant && (
-            <InfoWindow
-               position={{
-                  lat: selectedRestaurant.lat,
-                  lng: selectedRestaurant.long
+      <Fragment>
+         <GoogleMap defaultZoom={8} defaultCenter={coords}>
+            {/* display the markers */}
+            {data.map((resto, index) => {
+               return (
+                  <Marker
+                     key={resto.restaurantName}
+                     position={{
+                        lat: resto.lat,
+                        lng: resto.long
+                     }}
+                     onClick={() => handleClick(index)}
+                  />
+               );
+            })}
+            <Marker
+               key="positionUser"
+               position={coords}
+               icon={{
+                  url: UserIcon,
+                  scaledSize: new window.google.maps.Size(40, 40)
                }}
-               onCloseClick={() => setSelectedRestaurant(null)} //putting the value to no restaurant selected
-            >
-               <div>
-                  {/* Content of the InfoWindow modal */}
-                  {/* Header */}
-                  <h4>{selectedRestaurant.restaurantName}</h4>
-                  <p>{selectedRestaurant.address}</p>
-                  {/* Average rating section */}
-                  <DivNoteGenerale>
-                     <h5 style={{ margin: "0 10px 0 0" }}>Note générale :</h5>
-                     <Stars
-                        numberOfStars={averageStars(selectedRestaurant.ratings)}
-                        isEditable={false}
-                        size={25}
-                     />
-                  </DivNoteGenerale>
-                  {/* User rating section */}
+            />
+            {/* If the user selected a restaurant, we display the google modal : */}
+            {selectedRestaurant && (
+               <InfoWindow
+                  position={{
+                     lat: selectedRestaurant.lat,
+                     lng: selectedRestaurant.long
+                  }}
+                  onCloseClick={() => setSelectedRestaurant(null)} //putting the value to no restaurant selected
+               >
                   <div>
-                     {selectedRestaurant.ratings.map((resto, index) => {
-                        let i = index + 1; //to not start at 0
-                        console.log(i);
+                     {/* Content of the InfoWindow modal */}
+                     {/* Header */}
+                     <h4>{selectedRestaurant.restaurantName}</h4>
+                     <p>{selectedRestaurant.address}</p>
+                     {/* Average rating section */}
+                     <DivNoteGenerale>
+                        <h5 style={{ margin: "0 10px 0 0" }}>
+                           Note générale :
+                        </h5>
+                        <Stars
+                           numberOfStars={averageStars(
+                              selectedRestaurant.ratings
+                           )}
+                           isEditable={false}
+                           size={25}
+                        />
+                     </DivNoteGenerale>
+                     {/* User rating section */}
+                     <div>
+                        {selectedRestaurant.ratings.map((resto, index) => {
+                           let i = index + 1; //to not start at 0
+                           console.log(i);
 
-                        return (
-                           <Fragment key={resto.comment}>
-                              <DivAvis>
-                                 <PWrapper>Avis numéro {i} :</PWrapper>
-                                 <Stars
-                                    numberOfStars={resto.stars}
-                                    isEditable={false}
-                                    size={20}
-                                 />
-                              </DivAvis>
+                           return (
+                              <Fragment key={resto.comment}>
+                                 <DivAvis>
+                                    <PWrapper>Avis numéro {i} :</PWrapper>
+                                    <Stars
+                                       numberOfStars={resto.stars}
+                                       isEditable={false}
+                                       size={20}
+                                    />
+                                 </DivAvis>
 
-                              <p>{resto.comment}</p>
+                                 <p>{resto.comment}</p>
 
-                              {/* We don't want to display an <hr /> after the last element. */}
-                              {i === selectedRestaurant.ratings.length ? (
-                                 ""
-                              ) : (
-                                 <hr />
-                              )}
-                           </Fragment>
-                        );
-                     })}
+                                 {/* We don't want to display an <hr /> after the last element. */}
+                                 {i === selectedRestaurant.ratings.length ? (
+                                    ""
+                                 ) : (
+                                    <hr />
+                                 )}
+                              </Fragment>
+                           );
+                        })}
+                     </div>
+                     <div
+                        style={{
+                           width: "50rem",
+                           height: "400px",
+                           marginTop: "50px"
+                        }}
+                     >
+                        <h5>Aperçu :</h5>
+                        <ReactStreetview
+                           apiKey={API_KEY}
+                           streetViewPanoramaOptions={{
+                              position: {
+                                 lat: selectedRestaurant.lat,
+                                 lng: selectedRestaurant.long
+                              },
+                              pov: { heading: 100, pitch: 0 },
+                              zoom: 1
+                           }}
+                        />
+                     </div>
                   </div>
-               </div>
-            </InfoWindow>
-         )}
-      </GoogleMap>
+               </InfoWindow>
+            )}
+         </GoogleMap>
+      </Fragment>
    );
 };
 
