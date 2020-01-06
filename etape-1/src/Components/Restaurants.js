8554fbe9d7777ff35223edc3e8e13a07fcd3c0ba
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2020-01-07 00:10:26 +0100

    modifying the structure

--- etape-1/src/Components/Restaurants.js
+++ etape-1/src/Components/Restaurants.js
@@ -2,7 +2,7 @@ import React from "react";
 import data from "../data.json";
 import Stars from "./Common/Stars";
 import { averageStars } from "../services/libs";
-import Filter from "./Filter";
+import Filter from "./Common/Filter";
 export const Restaurants = () => {
    const [restaurants, setRestaurants] = React.useState(data);
    const [minimum, setMinimum] = React.useState(null);
