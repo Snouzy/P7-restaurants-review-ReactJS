e787ba3ec9298c5d8a7f803cc5bc8f3babd63190
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2020-01-14 16:09:48 +0100

    comments & ratings are now responsives

--- etape-3/src/Components/Common/UserReview.js
+++ etape-3/src/Components/Common/UserReview.js
@@ -1,27 +1,37 @@
-import React from "react";
-import styled from "styled-components";
-import Stars from "./Stars";
+import React from 'react';
+import styled from 'styled-components';
+import Stars from './Stars';
 
 export const UserReview = ({ numero, resto }) => {
    const thisComment = resto.ratings[numero];
    return (
       <>
          <DivAvis>
-            <PTime> ({thisComment.relative_time_description})</PTime>
-            <PNameWrapper>{thisComment.author_name} :</PNameWrapper>
-            <Stars
-               numberOfStars={thisComment.rating}
-               isEditable={false}
-               size={20}
-            />
+            <div className="row">
+               <div className="col-sm-12">
+                  <PTime> ({thisComment.relative_time_description})</PTime>
+               </div>
+
+               <DivNameAndStars className="col-sm-12">
+                  <PName>{thisComment.author_name} :</PName>
+                  <div style={{ display: 'inline-block' }}>
+                     <Stars
+                        numberOfStars={thisComment.rating}
+                        isEditable={false}
+                        size={20}
+                     />
+                  </div>
+               </DivNameAndStars>
+            </div>
          </DivAvis>
+
          <DivComment>
             <ImgProfile src={thisComment.profile_photo_url} />
             <p style={{ margin: 0 }}>
-               {" "}
+               {' '}
                {thisComment.text.length
                   ? thisComment.text
-                  : "L'auteur n'a pas donné d'avis."}{" "}
+                  : "L'auteur n'a pas donné d'avis."}{' '}
             </p>
          </DivComment>
 
@@ -30,19 +40,24 @@ export const UserReview = ({ numero, resto }) => {
    );
 };
 
-const PNameWrapper = styled.p`
+const DivNameAndStars = styled.div`
+   display: flex;
+   align-items: center;
+`;
+const PName = styled.p`
    margin: 0 10px 0 0;
    font-weight: bold;
+   display: inline;
 `;
 
 const DivAvis = styled.div`
-   display: flex;
-   align-items: center;
+   ${'' /* display: flex; */}
+   ${'' /* align-items: center; */}
    margin: 10px 0;
 `;
 
 const PTime = styled.p`
-   margin: 0 7px 0 0;
+   ${'' /* margin: 0 7px 0 0; */}
    font-style: italic;
 `;
 
