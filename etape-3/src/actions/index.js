7318c8d17282b4f9b36c346d414992faf98d2544
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2020-01-16 19:42:59 +0100

    only testing

--- etape-3/src/actions/index.js
+++ etape-3/src/actions/index.js
@@ -34,14 +34,18 @@ function formatRestaurant(r) {
 export const updateUserPosition = position => async dispatch => {
    dispatch({ type: 'UPDATE_USER_POSITION', payload: position });
 
-   const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position.lat},${position.lng}&radius=10000&type=restaurant&key=${API_KEY}`);
+   const res = await axios.get(
+      `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position.lat},${position.lng}&radius=10000&type=restaurant&key=${API_KEY}`
+   );
    const data = res.data;
 
    data.results.map(async r => {
-      const details = await axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${r.place_id}&fields=place_id,name,rating,formatted_address,review,geometry&key=${API_KEY}`)
+      const details = await axios.get(
+         `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${r.place_id}&fields=place_id,name,rating,formatted_address,review,geometry&key=${API_KEY}`
+      );
       dispatch({
          type: 'UPDATE_RESTAURANTS',
          payload: formatRestaurant(details.data.result)
       });
-   })
+   });
 };
